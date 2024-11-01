9f371e4506a64a159bf830d226c0bc408405caa9
Fredrik Burmester <fredrik.burmester@gmail.com>
2024-11-01 09:08:43 +0100

    fix: fetch correct server

--- app/app/(app)/servers/[id]/(auth)/history/page.tsx
+++ app/app/(app)/servers/[id]/(auth)/history/page.tsx
@@ -1,18 +1,23 @@
 import { Container } from "@/components/Container";
-import { getServers, getStatisticsHistory } from "@/lib/db";
-import { HistoryTable } from "./HistoryTable";
 import { PageTitle } from "@/components/PageTitle";
+import { getServer, getStatisticsHistory } from "@/lib/db";
 import { redirect } from "next/navigation";
+import { HistoryTable } from "./HistoryTable";
 
-export default async function HistoryPage() {
-  const servers = await getServers();
-  const server = servers?.[0];
+export default async function HistoryPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const server = await getServer(id);
 
   if (!server) {
     redirect("/setup");
   }
 
   const data = await getStatisticsHistory(server.id);
+
   return (
     <Container>
       <PageTitle title="History" />
