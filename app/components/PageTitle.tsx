86dacfe01fc702d5439564953f87f53ab39b150d
Fredrik Burmester <fredrik.burmester@gmail.com>
2024-11-06 16:44:12 +0000

    chore

--- app/components/PageTitle.tsx
+++ app/components/PageTitle.tsx
@@ -7,7 +7,11 @@ export const PageTitle: React.FC<PageTitleProps> = ({ title, subtitle }) => {
   return (
     <div className="flex flex-col mb-6">
       <h1 className="font-bold text-3xl">{title}</h1>
-      {subtitle && <p className="text-neutral-500 text-sm">{subtitle}</p>}
+      {subtitle && (
+        <p className="text-neutral-500 text-sm text-balance max-w-[50ch]">
+          {subtitle}
+        </p>
+      )}
     </div>
   );
 };
