fc4c2e0e62d0095a889cddb8a331c8220ac0e15b
Fredrik Burmester <fredrik.burmester@gmail.com>
2025-04-04 11:02:39 +0200

    fix: skip folders

--- server/lib/streamystat_server/jellyfin/client.ex
+++ server/lib/streamystat_server/jellyfin/client.ex
@@ -109,7 +109,8 @@ defmodule StreamystatServer.Jellyfin.Client do
       Fields: Enum.join(@default_item_fields, ","),
       StartIndex: start_index,
       Limit: limit,
-      EnableImageTypes: "Primary,Backdrop,Banner,Thumb"
+      EnableImageTypes: "Primary,Backdrop,Banner,Thumb",
+      IsFolder: false
     }
 
     # Add ImageTypes parameter if provided
