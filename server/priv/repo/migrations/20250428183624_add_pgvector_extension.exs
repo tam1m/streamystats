3579dfb5cd200110b70903d05b93af0c1c10e476
Fredrik Burmester <fredrik.burmester@gmail.com>
2025-05-17 22:39:17 +0200

    fix: correct extension name

--- server/priv/repo/migrations/20250428183624_add_pgvector_extension.exs
+++ server/priv/repo/migrations/20250428183624_add_pgvector_extension.exs
@@ -2,6 +2,6 @@ defmodule StreamystatServer.Repo.Migrations.AddPgvectorExtension do
   use Ecto.Migration
 
   def change do
-    execute("CREATE EXTENSION IF NOT EXISTS vector")
+    execute("CREATE EXTENSION IF NOT EXISTS vectors")
   end
 end
