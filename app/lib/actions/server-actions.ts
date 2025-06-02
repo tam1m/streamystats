2a9d9dd8c362508169a6cbf9231923ddb070f79f
Fredrik Burmester <fredrik.burmester@gmail.com>
2025-06-02 16:40:41 +0200

    chore

--- app/lib/actions/server-actions.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-
-import { setPreferredServer as setPreferredServerCookie } from "@/lib/preferred-server";
-
-export const setPreferredServerAction = async (
-  serverId: number
-): Promise<void> => {
-  await setPreferredServerCookie(serverId);
-};
