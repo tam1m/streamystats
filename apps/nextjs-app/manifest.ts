e6e542d3cbdc841143a1a2ec2c636250cd51a1d5
Jorge Schrauwen <sjorge@blackdot.be>
2025-07-23 13:12:01 +0200

    feat: make middleware aware of basePath
    Companion to the next.config.mjs change, this makes the middleware (and manifest) basePath ware.
    This is sadly only evaluate at runtime so the NEXT_PUBLIC_BASE_PATH needs to match what was used during `pnpm build`.

--- apps/nextjs-app/manifest.ts
+++ apps/nextjs-app/manifest.ts
@@ -1,3 +1,4 @@
+import { basePath } from "@/lib/utils";
 import type { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
@@ -5,18 +6,18 @@ export default function manifest(): MetadataRoute.Manifest {
     name: "Streamystats",
     short_name: "Streamystats",
     description: "A statistics service for Jellyfin.",
-    start_url: "/",
+    start_url: `${basePath}/`,
     display: "standalone",
     background_color: "#000",
     theme_color: "#1C4ED8",
     icons: [
       {
-        src: "/web-app-manifest-192x192.png",
+        src: `${basePath}/web-app-manifest-192x192.png`,
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/web-app-manifest-512x512.png",
+        src: `${basePath}/web-app-manifest-512x512.png`,
         sizes: "512x512",
         type: "image/png",
       },
